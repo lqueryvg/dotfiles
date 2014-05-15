1aa4c1f7b91844f9d9f0a9a9e0f1c52f4c490c48
lqueryvg <lqueryvg@gmail.com>
2014-05-15 17:33:00 +0100

    adding quiz

---
+++ bin/quiz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Generate some questions')
+parser.add_argument('filename', nargs=1)
+args = parser.parse_args()
+print(args.filename)
+
+file_content = open(args.filename)
+
