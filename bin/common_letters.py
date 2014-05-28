c2c9fd3dad62d3c41ecbadfb933240ec465630f2
lqueryvg <lqueryvg@gmail.com>
2014-05-28 11:17:10 +0100

    simplified

--- bin/common_letters.py
+++ bin/common_letters.py
@@ -6,8 +6,8 @@ import collections
 parser = argparse.ArgumentParser(description='''
     Reads list of strings from a file, one per line, and prints
     a list of characters that are common to all strings.
-    Repeat characters are counted so for example the two strings
-  "abbccc", "abcczz" would have and "abcc" in common.
+    Repeat characters are not condensed, so for example the two strings
+    "abbccc", "abcczz" would have and "abcc" in common.
                                  ''')
 parser.add_argument('filename', nargs=1)
 args = parser.parse_args()
@@ -18,19 +18,16 @@ except IOError:
     print("failed to open file '" + args.filename[0] + "'")
     exit(-1)
 
-def commonLetters(*strings):
-    result = None
+def commonLetters(strings):
+    result = collections.Counter(strings.pop(0))
     for str in strings:
-        if result:
-            result = result & collections.Counter(str)
-        else:
-            result = collections.Counter(str)
+        # intersection
+        result = result & collections.Counter(str)
     return result.elements()
-
 
 strings = file_obj.read().splitlines()
 
 print("The following letters are common in all lines of the file:")
-print("<" + "".join(sorted(commonLetters(*strings))) + ">")
+print("<" + "".join(sorted(commonLetters(strings))) + ">")
 
 exit(0)
