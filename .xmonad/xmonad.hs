ccd042c7b6c29b4c3bcd57fda0fd8136245046e4
lqueryvg <lqueryvg@gmail.com>
2014-11-19 22:57:53 +0000

    added xmonad

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,28 @@
+import XMonad
+--import XMonad.Layout.SimpleFloat
+import XMonad.Layout.ThreeColumns
+import XMonad.Hooks.DynamicLog
+
+-- Define default layouts used on most workspaces
+--defaultLayouts = tiled ||| simpleFloat ||| Full
+--defaultLayouts = tiled ||| ThreeCol 1 (3/100) (1/2) ||| Full
+myLayouts = ThreeCol 1 (1/100) (1/2) ||| Full
+--    where
+        -- default tiling algorithm partitions the screen into two panes
+--        tiled   = Tall nmaster delta ratio
+        -- The default number of windows in the master pane
+--        nmaster = 1
+        -- Default proportion of screen occupied by master pane
+--        ratio   = 1/2
+        -- Percent of screen to increment by when resizing panes
+--        delta   = 3/100
+
+-- Put all layouts together
+--myLayouts = defaultLayouts
+
+-- Run XMonad
+main = do
+--      xmonad $ xmobar defaultConfig {
+      xmonad =<< xmobar defaultConfig {
+        layoutHook = myLayouts
+      }
