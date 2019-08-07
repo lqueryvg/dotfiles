6b349811b86c8b94eb99a65dc86cd307e5b2132b
John Buxton <lqueryvg@gmail.com>
2019-08-07 08:46:19 +0100

    ssm functions & AWS_SDK_LOAD_CONFIG for working with serverless

--- profile.d/aws.sh
+++ profile.d/aws.sh
@@ -25,42 +25,23 @@ function ssmputs() {  # s for secure!
   aws ssm put-parameter --name $1 --value $2 --type SecureString --overwrite
 }
 
+function ssmputsFile() {  # s for secure!
+  set -x
+  aws ssm put-parameter --name $1 --value "$(cat $2)" --type SecureString --overwrite
+}
+
 function ssmdel() {
   set -x
   aws ssm delete-parameter --name $1
 }
 
-function ssmcp() {
-  [[ $# == 2 ]] || {
-    echo "Usage:"
-    echo "  source-param target-param"
-    echo "  source-param aws-profile:"
-    echo "  source-param aws-profile:target-param"
-    return 1
-  }
-  source_param=$1
-  if [[ $2 == *: ]]; then
-    target_profile=${2%:}
-    target_param=$source_param
-  elif [[ $2 == *:* ]]; then
-    target_profile=${2//:*/}
-    target_param=${2//*:/}
-  else
-    target_profile=$AWS_PROFILE
-    target_param=$2
-  fi
-
-  echo $source_param $target_profile $target_param
-
-  aws ssm get-parameters --names $source_param \
-    --with-decryption                          \
-    --query 'Parameters[0].[Value,Type]'       \
-    --output text | read value type
-  [[ $value == None ]] && {
-    echo "unable to get value of $source_param"
-    return 1
-  }
+function ssmdump() {
   set -x
-  echo AWS_PROFILE=$target_profile aws ssm put-parameter --name $target_param \
-    --value $value --type $type --overwrite
+  aws ssm get-parameters-by-path --path / --with-decryption --recursive
 }
+
+function ssmdescribe() {
+  set -x
+  aws ssm describe-parameters   --query 'Parameters[*].[Name,Description]' --output table
+}
+
