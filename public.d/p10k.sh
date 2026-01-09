8e34fd7e7118b80a6a04af24fb483aed6fbeaae8
John Buxton <lqueryvg@gmail.com>
2026-01-09 14:29:19 +0000

    move p10k out of .zshrc

---
+++ public.d/p10k.sh
@@ -0,0 +1,5 @@
+# powerlevel10k prompt
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+zinit ice depth=1; zinit light romkatv/powerlevel10k
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
